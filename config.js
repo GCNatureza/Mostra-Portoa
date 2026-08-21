03b06c4274f02595bb35101544a8905aaf488eac
GCNatureza <prof.alex.vieira.fisica@gmail.com>
2026-08-21 17:50:38 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,11 +3,11 @@
 
    API_URL é o endereço do aplicativo web do Google Apps Script
    (termina em /exec). Ele muda apenas se você criar uma implantação
-   NOVA; ao publicar uma nova versão da implantação existente, o
-   endereço continua o mesmo.
+   NOVA; ao publicar uma nova versão da implantação já existente, o
+   endereço continua o mesmo e este arquivo não precisa ser alterado.
    ------------------------------------------------------------------ */
 
 window.CONFIG = {
   API_URL:
-    "https://script.google.com/macros/s/AKfycbyaqSmCi4ol_cFP3AgpUkeZE0v3OLqRHDEL9w_P6OExznrWQZRVtGSM1mCgyslKM_n3pg/exec",
+    "https://script.google.com/macros/s/AKfycbwm4vRdW7wYD-FfSRBk-gV5QpeV4aEAvMRqQVZxUwxAK9iS5LnLkQho2QipQHgPkjT9yQ/exec",
 };
